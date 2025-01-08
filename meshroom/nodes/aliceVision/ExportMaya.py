af1cda0c916eb40f19ed082fd12e0a77634331ae
Candice Bentéjac <candice.bentejac@gmail.com>
2025-01-08 18:59:49 +0100

    [nodes] Replace `desc.Node.internalFolder` with `{nodeCacheFolder}`

--- meshroom/nodes/aliceVision/ExportMaya.py
+++ meshroom/nodes/aliceVision/ExportMaya.py
@@ -36,6 +36,6 @@ MeshroomMaya contains a user interface to browse all cameras.
             name="output",
             label="Folder",
             description="Folder for MeshroomMaya outputs: undistorted images and thumbnails.",
-            value=desc.Node.internalFolder,
+            value="{nodeCacheFolder}",
         ),
     ]
